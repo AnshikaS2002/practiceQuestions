5d52eea52a9024e5b48631739d99f3b51b5c0451
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-25 09:02:16 +0530

    string concatenation and repeated chars check

--- Demo.java
+++ Demo.java
@@ -1,9 +1,39 @@
 ///Write a Java program that takes in an array of strings and concatenates them into a single string.
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Demo {
+
+    String concatenate(ArrayList<String> arr) {
+        String concatenatedString = "";
+
+        for (String s : arr) {
+            concatenatedString += s;
+            concatenatedString += " ";
+        }
+        return concatenatedString;
+    }
+
+    String repeatedCharacters(String concatenatedString) {
+
+        String repeatStrings = "";
+        HashSet<String> set = new HashSet();
+        StringBuffer sb = new StringBuffer();
+        sb.append(concatenatedString);
+
+        for (int i = 0; i < sb.length(); i++) {
+            String s = sb.substring(i, i + 1);
+            if (set.contains(s)) {
+                repeatStrings += s;
+            } else {
+                set.add(s);
+            }
+        }
+        return repeatStrings;
+    }
+
     public static void main(String[] args) {
         ArrayList<String> arr = new ArrayList<>();
         int cnt = 0;
@@ -18,14 +48,11 @@ public class Demo {
             arr.add(curString);
         }
 
-        String concatenatedString = "";
-
-        for (String s : arr) {
-            concatenatedString += s;
-            concatenatedString += " ";
-        }
-
+        Demo demo = new Demo();
+        String concatenatedString = demo.concatenate(arr);
         System.out.println("Concatenated string is " + concatenatedString);
+        String repeatedString = demo.repeatedCharacters(concatenatedString);
+        System.out.println("Repeated characters are : " + repeatedString);
         scanner.close();
     }
 }
